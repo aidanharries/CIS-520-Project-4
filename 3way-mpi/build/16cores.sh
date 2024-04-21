9c4ef023ceaaa35d1ab006cae307b2ab19b8ae8b
aidanharries <aidanharries@gmail.com>
2024-04-21 18:03:11 -0500

    Added shell programs for MPI

--- 3way-mpi/build/16cores.sh
+++ 3way-mpi/build/16cores.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #SBATCH --mem=16G
 #SBATCH --time=24:00:00
-#SBATCH --job-name=1core
+#SBATCH --job-name=16cores
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=16
 #SBATCH --nodelist=mole[001-040,053-079,081-120]
 
 max_lines=100   # Specify the number of lines you want to process here
@@ -11,6 +11,6 @@ max_lines=100   # Specify the number of lines you want to process here
 echo "Running MPI max char finder on $HOSTNAME"
 echo "" # New line
 
-mpirun -np 1 ./mpi_program /homes/dan/625/wiki_dump.txt $max_lines
+mpirun -np 16 ./mpi_program /homes/dan/625/wiki_dump.txt $max_lines
 
 echo "Finished run on $SLURM_NTASKS $HOSTNAME cores"
